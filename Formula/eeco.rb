840abeea3bb4bed361cad116d481f1189babe7bd
Anton <anton.j.hahn@icloud.com>
2026-05-30 02:13:10 +0000

    eeco v1.14.0: sync eeco.rb

--- Formula/eeco.rb
+++ Formula/eeco.rb
@@ -1,26 +1,26 @@
 class Eeco < Formula
   desc "Self-maintaining workflow ecosystem for a coding project"
   homepage "https://github.com/ajhahnde/eeco"
-  version "1.13.0"
-  license "Apache-2.0"
+  version "1.14.0"
+  license "PolyForm-Noncommercial-1.0.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ajhahnde/eeco/releases/download/v1.13.0/eeco_v1.13.0_darwin_arm64.tar.gz"
-      sha256 "2f138303bad1078d7525ae4d69c9903506733d65165ad6130e3f72494059f73e"
+      url "https://github.com/ajhahnde/eeco/releases/download/v1.14.0/eeco_v1.14.0_darwin_arm64.tar.gz"
+      sha256 "11c4a89d7401566d1946e72a9d7003e1d4277cc81c1ea836ebd156cbd3472353"
     else
-      url "https://github.com/ajhahnde/eeco/releases/download/v1.13.0/eeco_v1.13.0_darwin_amd64.tar.gz"
-      sha256 "ba2aa2159e840a05de0e5ea66b8c06c3b5b24bc979d46887b77739735a912817"
+      url "https://github.com/ajhahnde/eeco/releases/download/v1.14.0/eeco_v1.14.0_darwin_amd64.tar.gz"
+      sha256 "59adf1c1275188e98d1f9e1cff241c75311cd16882fcfb3b0e4e2859aa784c32"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/ajhahnde/eeco/releases/download/v1.13.0/eeco_v1.13.0_linux_arm64.tar.gz"
-      sha256 "7cdb78cc32b01095e03b098a26cacc78184ae60926628cb74d0b6e8d61e2fb2d"
+      url "https://github.com/ajhahnde/eeco/releases/download/v1.14.0/eeco_v1.14.0_linux_arm64.tar.gz"
+      sha256 "e73616c1d3fa8f5b35a9e7c611da013162f87b0c95c4ebdcef202407485a3fc2"
     else
-      url "https://github.com/ajhahnde/eeco/releases/download/v1.13.0/eeco_v1.13.0_linux_amd64.tar.gz"
-      sha256 "0e8a212b5c08a8ce5d3cd0b11297880a08351bac9850c8c24d3b8615b62873ea"
+      url "https://github.com/ajhahnde/eeco/releases/download/v1.14.0/eeco_v1.14.0_linux_amd64.tar.gz"
+      sha256 "6e07b9c993ab14d65f1a6717672048457aa1843bd29fd105fd01e34a208e9268"
     end
   end
 
