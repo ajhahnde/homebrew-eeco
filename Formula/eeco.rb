54776579366d3de02b8ef36756c6776dc73bfe41
Anton <anton.j.hahn@icloud.com>
2026-05-30 18:40:40 +0000

    eeco v1.16.0: sync eeco.rb

--- Formula/eeco.rb
+++ Formula/eeco.rb
@@ -1,26 +1,26 @@
 class Eeco < Formula
   desc "Self-maintaining workflow ecosystem for a coding project"
   homepage "https://github.com/ajhahnde/eeco"
-  version "1.15.0"
+  version "1.16.0"
   license "PolyForm-Noncommercial-1.0.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ajhahnde/eeco/releases/download/v1.15.0/eeco_v1.15.0_darwin_arm64.tar.gz"
-      sha256 "3071568d194bf52f16c76246a404ba65d3572a2f38aac29d0c5dbac55645b279"
+      url "https://github.com/ajhahnde/eeco/releases/download/v1.16.0/eeco_v1.16.0_darwin_arm64.tar.gz"
+      sha256 "496bd9da004ebaccab73383d38e7262985b258aee33d686dcbc09db489646d79"
     else
-      url "https://github.com/ajhahnde/eeco/releases/download/v1.15.0/eeco_v1.15.0_darwin_amd64.tar.gz"
-      sha256 "e767c07c6ce89e5e139b6cd8b85606a6b6d4d893402bdd8a743b67c10088c0b0"
+      url "https://github.com/ajhahnde/eeco/releases/download/v1.16.0/eeco_v1.16.0_darwin_amd64.tar.gz"
+      sha256 "460c7dbdece57e65133c41f1b7e07b6a405fc9d5e88405ec0284a49b5c765449"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/ajhahnde/eeco/releases/download/v1.15.0/eeco_v1.15.0_linux_arm64.tar.gz"
-      sha256 "2c0577a7edde2be88f9bfb8efdc78888656618e4ca734047308668cc80278c35"
+      url "https://github.com/ajhahnde/eeco/releases/download/v1.16.0/eeco_v1.16.0_linux_arm64.tar.gz"
+      sha256 "a842de1d98b19c6c2480c58fec4c97bacf4d0328c7e5d1cfdff8ca554ad8d7dc"
     else
-      url "https://github.com/ajhahnde/eeco/releases/download/v1.15.0/eeco_v1.15.0_linux_amd64.tar.gz"
-      sha256 "65506959a0a51d3988e600900bd9353cf67e5079dabfcf431a6de2f8b147232d"
+      url "https://github.com/ajhahnde/eeco/releases/download/v1.16.0/eeco_v1.16.0_linux_amd64.tar.gz"
+      sha256 "e799f1da4c4018c96db149ff3592d7d2c3b439f84178c913002c7f3a4b9e6e5c"
     end
   end
 
